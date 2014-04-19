ab810c4b11c0ad41678187bd0bc927a429357266
Supersonic112 <supersonic112@live.de>
2014-04-19 21:52:33 +0200

    added most of the sound effects to the sound init file

--- love2d/soundinit.lua
+++ love2d/soundinit.lua
@@ -3,10 +3,18 @@ require "sound"
 function love.sounds.initSounds()
 	----envSounds:----------------------------
 	--parameters: name,location,isLoop,loopsPerPlay (if set to nil while isLoop is true, the sound will have to be stopped manually)
-	love.sounds.addEnvSoundFile("birds","res/sfx/birds.mp3",true,1)
-	love.sounds.addEnvSoundFile("windyCave","res/sfx/windy_cave.mp3",true,-1)
-	love.sounds.addEnvSoundFile("fireplace","res/sfx/fireplace.mp3",true,-1)
+	love.sounds.addEnvSoundFile("bird","res/sfx/bird.mp3",true,nil)
+	love.sounds.addEnvSoundFile("birds","res/sfx/birds.mp3",true,nil)
+	love.sounds.addEnvSoundFile("caveAtmo","res/sfx/cave_atmo.mp3",true,nil)
+	love.sounds.addEnvSoundFile("darkAtmo2","res/sfx/dark_atmo_2.mp3",true,nil)
+	love.sounds.addEnvSoundFile("darkAtmo3","res/sfx/dark_atmo_3.mp3",true,nil)
+	love.sounds.addEnvSoundFile("distortedAtmo","res/sfx/distorted_atmo.mp3",true,nil)
+	love.sounds.addEnvSoundFile("fireplace","res/sfx/fireplace.mp3",true,nil)
+	love.sounds.addEnvSoundFile("riverLoop1","res/sfx/river_loop_1.mp3",true,nil)
 	love.sounds.addEnvSoundFile("spell","res/sfx/spell.mp3",false)
+	love.sounds.addEnvSoundFile("tensionAtmo","res/sfx/tension_atmo.mp3",true,nil)
+	love.sounds.addEnvSoundFile("waterBirdsEnvironment","res/sfx/water_birds_environment.mp3",true,nil)
+	love.sounds.addEnvSoundFile("windyCave","res/sfx/windy_cave.mp3",true,nil)
 	----Background music:---------------------
 	--parameters: name,location,isLoop
 	love.sounds.addBgmSoundFile("desertCowboy","res/music/desert_cowboy.mp3",true)
