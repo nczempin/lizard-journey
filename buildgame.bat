583d472c962d9e9b40e74d8c36b2d3b8b1c856e6
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-04-20 22:08:25 +0200

    update build script

--- buildgame.bat
+++ buildgame.bat
@@ -1,13 +1,14 @@
 
 @ECHO OFF
-md buildtmp
-::copy localconfig.lua.dist "%~dp0\buildtmp\localconfig.lua"
-copy readme.md "%~dp0\buildtmp\readme.txt"
-"D:\Program Files\7-Zip\7z" -r a -tzip .\buildtmp\game.love .\love2d\*.*
-copy "buildmaterial\lovewin64\*.*" "buildtmp\"
-copy /b "buildtmp\love.exe"+"buildtmp\game.love" "buildtmp\tm.exe"
-del ".\buildtmp\game.love"
-del ".\buildtmp\love.exe"
-"D:\Program Files\7-Zip\7z" -r a -tzip .\tm.zip .\buildtmp\*.*
-rd /s /q buildtmp
+
+set foldername=lizards-journey-v0.3.1
+md %foldername%
+copy readme.md "%~dp0\%foldername%\readme.txt"
+"D:\Program Files\7-Zip\7z" -r a -tzip .\%foldername%\game.love .\love2d\*.*
+copy "buildmaterial\lovewin64\*.*" "%foldername%\"
+copy /b "%foldername%\love.exe"+"%foldername%\game.love" "%foldername%\%foldername%.exe"
+del ".\%foldername%\game.love"
+del ".\%foldername%\love.exe"
+"D:\Program Files\7-Zip\7z" -r a -tzip .\%foldername%.zip .\%foldername%
+::rd /s /q %foldername%
 pause
