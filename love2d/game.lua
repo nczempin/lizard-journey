98336c4fcf0443df02292c9e8947a8cb4e968f1f
Francisco Pinto <me@f.rancis.co>
2014-04-22 01:30:35 +0200

    Added proper credits and slightly refactored game.lua (still many smells to fix)

--- love2d/game.lua
+++ love2d/game.lua
@@ -20,10 +20,36 @@ function love.game.newGame()
 		o.world.init()
 
 		o.setupMenu()
+		o.setupCredits()
 
 		o.setState(states.MAIN_MENU) -- set the starting state (use e. g. MAIN_MENU if you work on the menus)
 	end
+	
+	-- TODO own class for credits
+	o.setupCredits = function()
+		if not o.peopleTextual then
+			o.people = {
+				"Markus Vill", "Nicolai Czempin", "Bernd Hildebrandt",
+				"Marcus Ihde", "Meral Leyla", "Aldo Briessmann", 
+				"Sotos", "Terence-Lee Davis", "Francisco Pinto",
+			}
+			table.sort(o.people) -- 2lazy
 
+			o.peopleTextual = ""
+			for i, person in ipairs(o.people) do
+				o.peopleTextual = o.peopleTextual .. person
+				if i ~= #o.people then
+					o.peopleTextual = o.peopleTextual .. "\n\n"
+				end
+			end
+			
+			o.creditsFont = FONT_MEDIUM
+			local nLines = select(2, o.peopleTextual:gsub('\n', '\n'))
+			o.creditsHPos = W.getHeight()/2 - .75*nLines/2*o.creditsFont:getHeight()
+		end
+	end
+	
+	-- TODO move this method elsewhere
 	o.setupMenu = function()
 		o.menu = love.gui.newGui()
 
@@ -40,15 +66,14 @@ function love.game.newGame()
 		end
 		--]]
 
-
 		o.buttonImage = G.newImage("res/gfx/gui-arrow.png")
 		G.setFont(FONT_LARGE)
 		local padding = 7
 		local fontH = FONT_MEDIUM:getHeight()
 		local spacing = 25
 		local buttonH = padding + fontH
-		local buttonX = love.window.getWidth()/2 - 140/2
-		local windowH = love.window.getHeight()/2
+		local buttonX = W.getWidth()/2 - 140/2
+		local windowH = W.getHeight()/2
 		local nButtons = 3
 		local totalButtonH = nButtons*(spacing + buttonH)
 		o.playButton = o.menu.newButton(buttonX, windowH - totalButtonH/2 + 0*(spacing + buttonH), 140, buttonH, "Play", o.buttonImage)
@@ -88,11 +113,11 @@ function love.game.newGame()
 		if o.state == states.MAIN_MENU then
 			if o.playButton.hit then
 				o.setState(states.GAMEPLAY)
-				TEsound.stop(love.sounds.bgm.activeBgm,false)
-				--love.sounds.playBgm("lizardGuitarFx")
+				TEsound.stop(S.bgm.activeBgm,false)
+				--S.playBgm("lizardGuitarFx")
 			elseif o.creditsButton.hit then
 				o.setState(states.CREDITS)
-				love.sounds.playBgm("lizardGuitarSlow")
+				S.playBgm("lizardGuitarSlow")
 			elseif o.exitButton.hit then
 				love.event.quit()
 			end
@@ -101,44 +126,66 @@ function love.game.newGame()
 			o.world.update(dt)
 		end
 	end
+	
+	o.drawBackgroundImage = function()
+		G.setColor(255, 255, 255)
+		G.draw(o.backgroundImage, 0, 0, 0,
+			W.getWidth()/o.backgroundImage:getWidth(),
+			W.getHeight()/o.backgroundImage:getHeight())
+	end
+	
+	o.drawTitle = function(titleText)
+		local font = FONT_XLARGE
+		G.setFont(font)
+		G.setColor(120, 118, 112)
+		G.printf(titleText, 0, W.getHeight()/4 - font:getHeight()/2, W.getWidth(), "center")
+		G.setColor(255, 255, 255)
+	end
+	
+	o.drawCredits = function()
+		o.drawBackgroundImage()
+		o.drawTitle("Credits")
+		
+		local hh = W.getHeight()/2
+		local w = W.getWidth()
+		G.setFont(o.creditsFont)
+		G.setColor(81, 81, 81)
+		G.printf(o.peopleTextual, 0, o.creditsHPos, w, "center")
+		G.setColor(255, 255, 255)
+	end
+	
+	o.drawPause = function()
+		-- Draw world as backdrop
+		G.setColor(255, 255, 255, 255)
+		o.world.draw()
+		-- Draw transparent rectangle for the 'faded' effect
+		local w = W.getWidth()
+		local h = W.getHeight()
+		G.setColor(255, 255, 255, 96)
+		G.rectangle("fill", 0, 0, w, h)
+		-- Draw centered (H&V) text
+		G.setColor(0, 0, 0)
+		local font = FONT_XLARGE
+		G.setFont(font)
+		G.printf("Paused.", 0, h/2 - font:getHeight()/2, w, "center")
+
+		G.setColor(255, 255, 255)
+	end
 
 	o.draw = function()
 		if o.state == states.MAIN_MENU then
-			G.setColor(255, 255, 255)
-			G.draw(o.backgroundImage, 0, 0, 0,
-				love.window.getWidth()/o.backgroundImage:getWidth(),
-				love.window.getHeight()/o.backgroundImage:getHeight())
+			o.drawBackgroundImage()
 			o.menu.draw()
-
-			local font = FONT_XLARGE
-			G.setFont(font)
-			G.setColor(151*0.80, 147*0.80, 141*0.80)
-			G.printf("The Tale of Some Reptile", 0, love.window.getHeight()/4 - font:getHeight()/2, love.window.getWidth(), "center")
+			o.drawTitle("The Tale of Some Reptile")
 			-- debug
 			--G.setColor(255, 0, 0)
-			--G.rectangle("fill", love.window.getWidth()/2 - 20, love.window.getHeight()/2 - 20, 40, 40)
-
-			G.setColor(255, 255, 255)
+			--G.rectangle("fill", W.getWidth()/2 - 20, W.getHeight()/2 - 20, 40, 40)
 		elseif o.state == states.GAMEPLAY then
 			o.world.draw()
 		elseif o.state == states.CREDITS then
-			G.print("Credits!", o.x, o.y)
+			o.drawCredits()
 		elseif o.state == states.PAUSED then
-			-- Draw world as backdrop
-			G.setColor(255, 255, 255, 255)
-			o.world.draw()
-			-- Draw transparent rectangle for the 'faded' effect
-			local w = love.window.getWidth()
-			local h = love.window.getHeight()
-			G.setColor(255, 255, 255, 96)
-			G.rectangle("fill", 0, 0, w, h)
-			-- Draw centered (H&V) text
-			G.setColor(0, 0, 0)
-			local font = FONT_XLARGE
-			G.setFont(font)
-			G.printf("Paused.", 0, h/2 - font:getHeight()/2, w, "center")
-
-			G.setColor(255, 255, 255)
+			o.drawPause()
 		end
 	end
 
