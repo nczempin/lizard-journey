846039ff8fd735f4e696557cfa22dc656caf7377
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-04-22 13:12:23 +0200

    adapting the code a little bit for our needs. WARNING: this will break the game; I'm currently calling state.states directly with pure Lua.

--- love2d/state/states.lua
+++ love2d/state/states.lua
@@ -1,77 +1,50 @@
 
 -- GameStates:0=MainMenu, 1=inGame, 2=Load, 3=Settings, 4=Game Over, 5 = Credits
 states = {"main menu", "gameplay", "load", "settings", "game over", "credits", "settings/video","settings/video/advanced", "paused"}
-states.MAIN_MENU = states[1]
-states.GAMEPLAY = states[2]
+local mmState = {name= "main_menu"}
+mmState.action = function()
+	print "hello"
+end
+
+local gpState = {name = "gameplay"}
+states = {main_menu=mmState,gameplay=gpState}
+states.MAIN_MENU = states["main_menu"]
+states.GAMEPLAY = states["gameplay"]
 states.CREDITS = states[5]
 states.PAUSED = states[9]
 
 
 FSM = require "love2d/external/fsm"
 
-function action1() print("Performing action 1") end
+function action1() print("transition from mm to gp") end
 
-function action2() print("Performing action 2") end
+function action2() print("transition from gp to mm") end
 
-function action3() print("Action 3: Exception raised") end
-
-function action4() print("Wildcard in action !!!") end
 
 -- Define your state transitions here
 local myStateTransitionTable = {
-	{"state1", "event1", "state2", action1},
-	{"state2", "event2", "state3", action2}
+	{states.MAIN_MENU.name, "startGame", states.GAMEPLAY.name, action1},
+	{states.GAMEPLAY.name, "gotoMainMenu", states.MAIN_MENU.name, action2}
 }
 
 -- Create your instance of a finite state machine
 fsm = FSM.new(myStateTransitionTable)
 
--- print( "Constant UNKNOWN = " .. UNKNOWN )
-print( "Constant FSM.UNKNOWN = " .. FSM.UNKNOWN )
-
 -- Use your finite state machine
 -- which starts by default with the first defined state
-print("Current FSM state: " .. fsm:get())
+local name = fsm:get()
+print("Current FSM state: " .. name)
 
--- Or you can set another state
-fsm:set("state2")
-print("Current FSM state: " .. fsm:get())
-
+local state = states[name]
+if state and state.action then
+	state.action()
+end
 -- Respond on "event" and last set "state"
-fsm:fire("event2")
+local action = fsm:fire("startGame")
+fsm:fire("main_menu") --nothing should happen
+
 print("Current FSM state: " .. fsm:get())
 
--- Force exception
-print("Force exception by firing unknown event")
-fsm:fire("event3")
-print("Current FSM state: " .. fsm:get())
 
--- Test automated exception handling
-local myStateTransitionTable2 = {
-	{"state1", "event1", "state2", action1},
-	{"state2", "event2", "state3", action2},
-	{"*"     , "event3", "state1", action4},
-	{"*"     , "?",      "state1", action3}
-}
-
--- Create your instance of a finite state machine
-fsm2 = FSM.new(myStateTransitionTable2)
-fsm2:set("state2")
-print("\nCurrent FSM-2 state: " .. fsm2:get())
-
-fsm2:delete({{"state2", "event2"}} )
--- Force exception
-fsm2:fire("event2")
-
-fsm2:delete({{"*", "?"}} )
-print("Force third exception (silence = true)")
-fsm2:silent() -- prevent unknown state-event notificaton to be printed
-
-fsm2:fire("event3")
-print("Current FSM-2 state after firing  wildcard 'event3': " .. fsm2:get())
-
-fsm2:add({{"*"     , "*",      "state2", action3}})
-fsm2:fire("event2")
-print("Current FSM-2 state: " .. fsm2:get())
-print("Current FSM state: " .. fsm:get())
-
+state = fsm:get()
+print (state)
